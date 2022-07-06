1980cebdb7edaee7c9b49296963db19b9feb127c
tceyhan <55tc155@gmail.com>
2022-07-06 18:08:38 +0300

    paginate and card folllowers added

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Pagination from "react-bootstrap/Pagination";
 
-const Paginate = () => {
+const Paginate = ({pages, setCurrentPage}) => {
+
+  const [activePage, setActivePage] = useState(1)
+
+ 
+  let items = [];
+  for (let number = 1; number <= 5; number++) {
+    items.push(
+      <Pagination.Item key={number} active={number === activePage} onClick={() => setActivePage(number)}>
+        {number}
+      </Pagination.Item>
+    );
+  }
+
+  useEffect(() => {
+    setCurrentPage(activePage)
+  }, [activePage, setCurrentPage])
+  
   return (
-    <div>Paginate</div>
-  )
-}
+    <div>
+      <Pagination>{items}</Pagination>
+    </div>
+  );
+};
 
-export default Paginate
+export default Paginate;
--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-const SearchUser = () => {
+const SearchUser = ({handleChange}) => {
   return (
-    <div>SearchUser</div>
+    <div className='row'>
+     <div className='col-md-4 align-items-center'>
+     <InputGroup className="mb-3">
+        <InputGroup.Text id="inputGroup-sizing-default">
+         Search
+        </InputGroup.Text>
+        <Form.Control          
+          type='search'
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+          onChange={handleChange}
+        />
+      </InputGroup>
+     </div>
+    </div>
   )
 }
 
