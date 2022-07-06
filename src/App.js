0d6ce8d88d3971b7718969d3987bc8e0efc1982b
tceyhan <55tc155@gmail.com>
2022-07-06 16:18:12 +0300

    login form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      Welcome
+    <div className="container">
+      <AppRouter />
     </div>
   );
 }
