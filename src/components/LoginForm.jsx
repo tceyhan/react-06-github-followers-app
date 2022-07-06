611db1c6103ff6c888286871fdb07f9f1548a686
tceyhan <55tc155@gmail.com>
2022-07-06 16:28:33 +0300

    login form changed

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,14 +8,14 @@ const LoginForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     sessionStorage.setItem("email", email)
-
+    window.location.href="/home"
   }
   return (
     <div>
       <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" value={email} onChange ={(e) => setEmail(e.target.value)}required/>
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange ={(e) => setEmail(e.target.value)} required/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
